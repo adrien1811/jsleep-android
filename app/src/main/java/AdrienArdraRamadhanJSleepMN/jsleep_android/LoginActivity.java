3d8e4663945128cd395c9bde70e0266772fe06df
Adrien Ardra Ramadhan <adrien.ardra@ui.ac.id>
2022-11-15 17:56:43 +0700

    CS Modul 8

---
+++ app/src/main/java/AdrienArdraRamadhanJSleepMN/jsleep_android/LoginActivity.java
@@ -0,0 +1,36 @@
+package AdrienArdraRamadhanJSleepMN.jsleep_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+
+        TextView login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+    }
+}
